6dc4b668c0408a322f6ae1e6433401b7dae8a3f3
KyeongHa <rudgk030@hanyang.ac.kr>
2023-07-05 12:34:59 +0900

    2023-07-05

---
+++ src/main.js
@@ -0,0 +1,17 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+
+// we can use interacting the client with the backend services through the Amplify
+import { Amplify } from 'aws-amplify';
+import awsExports from './aws-exports';
+
+Amplify.configure(awsExports);
+
+const app = createApp(App)
+// app.config.isCustomElement = tag => tag.startsWith('amplify-');
+
+app.use(router)
+app.mount('#app')
