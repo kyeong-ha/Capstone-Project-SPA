8f7f3d4dcb0dcc9c37f27f6ed67cacb13b6882b3
KyeongHa <rudgk030@hanyang.ac.kr>
2023-07-09 17:14:33 +0900

    2023-07-09

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,3 @@
-<script src="./models/question_predefine.js"></script>
 <template>
   <div id="app">
     <h1>TEST App</h1>
@@ -6,10 +5,33 @@
     
     <p>{{ this.question }}</p>
     <input type="text" v-model="answer" placeholder="answer context" />
-    <button v-on:click="createForm()">Create Answer</button>
+    <!-- <button v-on:click="createForm()">Create Answer</button> -->
   </div>
 </template>
 
+<script>
+  import * as Mutation from './graphql/mutations';
+  import * as Query from './graphql/querys';
+  import * as Subscription from './graphql/subscriptions';
+  import { initQuestion } from './models/question_predefine'
+  
+  const questions = [
+      `At a party, would you rather spend most of the time talking to a few close friends or mingle with many different people?`,
+      `When deciding on a restaurant for dinner with friends, do you primarily consider the quality and taste of the food (logic) or whether everyone will feel comfortable and enjoy the atmosphere (emotions)?`,
+      `When planning a trip, do you focus on researching specific attractions and activities, like visiting historical landmarks, or do you look for overarching themes, like exploring local art and culture?`,
+      `Imagine you have a week-long project due next week. Would you create a detailed plan, breaking down the tasks day by day, or prefer to go with the flow and work on it when inspiration strikes?`,
+      `In a cooking class, would you rather learn through hands-on demonstrations, like preparing dishes yourself, or listening to explanations of different techniques and the science behind them?`,
+      `During a team-building activity, such as a group puzzle-solving game, do you take charge by assigning roles and directing the group or contribute your ideas and insights in a more collaborative manner?`,
+      `A friend comes to you with a personal dilemma. Do you offer a solution based on logical analysis, weighing the pros and cons, or do you focus on understanding and validating their feelings and concerns?`,
+      `You're in a disagreement with a colleague. Do you prioritize resolving the conflict by finding a compromise, even if it means not fully expressing your point of view, or do you assert your opinion even if it risks causing tension?`,
+      `On a weekend getaway, would you rather follow a detailed itinerary that includes a list of attractions, restaurants, and activities, or leave your schedule open to spontaneous adventures and discoveries?`,
+      `In your free time, are you more drawn to learning practical skills, like woodworking or gardening, or exploring abstract concepts and ideas, like philosophy or advanced mathematics?`
+  ];
+
+  for (var i = 1; i <= 10; i++) {
+    initQuestion(i, questions[i-1]);
+  }
+</script>
 
 <!-- <script>
 // import { RouterLink, RouterView } from 'vue-router'
--- src/models/question_predefine.js
+++ src/models/question_predefine.js
@@ -1,31 +1,22 @@
-import { DataStore } from "aws-amplify";
-// import amplifyConfig from "./aws-exports";
+import { DataStore } from 'aws-amplify';
+import { initSchema } from '@aws-amplify/datastore';
+import { schema } from './schema';
+const { Input } = initSchema(schema);
 
-const questions = [
-    `At a party, would you rather spend most of the time talking to a few close friends or mingle with many different people?`,
-    `When deciding on a restaurant for dinner with friends, do you primarily consider the quality and taste of the food (logic) or whether everyone will feel comfortable and enjoy the atmosphere (emotions)?`,
-    `When planning a trip, do you focus on researching specific attractions and activities, like visiting historical landmarks, or do you look for overarching themes, like exploring local art and culture?`,
-    `Imagine you have a week-long project due next week. Would you create a detailed plan, breaking down the tasks day by day, or prefer to go with the flow and work on it when inspiration strikes?`,
-    `In a cooking class, would you rather learn through hands-on demonstrations, like preparing dishes yourself, or listening to explanations of different techniques and the science behind them?`,
-    `During a team-building activity, such as a group puzzle-solving game, do you take charge by assigning roles and directing the group or contribute your ideas and insights in a more collaborative manner?`,
-    `A friend comes to you with a personal dilemma. Do you offer a solution based on logical analysis, weighing the pros and cons, or do you focus on understanding and validating their feelings and concerns?`,
-    `You're in a disagreement with a colleague. Do you prioritize resolving the conflict by finding a compromise, even if it means not fully expressing your point of view, or do you assert your opinion even if it risks causing tension?`,
-    `On a weekend getaway, would you rather follow a detailed itinerary that includes a list of attractions, restaurants, and activities, or leave your schedule open to spontaneous adventures and discoveries?`,
-    `In your free time, are you more drawn to learning practical skills, like woodworking or gardening, or exploring abstract concepts and ideas, like philosophy or advanced mathematics?`
-];
 
-// Amplify.configure(amplifyConfig);
+async function createOrUpdateQuestion(init_id, init_question) {
+    try{
+        const input = await DataStore.save(
+            new Input({
+                id: init_id.toString(),
+                question: init_question,
+                answer: ''
+            })
+        );
+        console.log('success', input);
+    } catch(error) {
+        console.log(error);
+    }
+}
 
-// function* question_init() {
-    // for(let i=1; i<=10; i++){
-            const input = await DataStore.save(
-                new Input({
-                    id: '1',
-                    question: questions[0],
-                    answer: ''
-                })
-            );
-// };
-
-// const init = question_initialized
-export default input;
+export {createOrUpdateQuestion};
